275741ce137dfd074dacca93934804472f54bc34
block36_ <block36not33@gmail.com>
2024-01-21 10:45:03 -0500

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
             create<MavenPublication>("maven") {
                 groupId = "celestialexpressions"
                 artifactId = "celestialexpressions"
-                version = "1.3"
+                version = "1.3.1"
 
                 from(components["java"])
             }
