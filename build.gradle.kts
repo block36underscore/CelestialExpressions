740d76e960237574165219715050a63c10a72472
fishcute <47741160+fishcute@users.noreply.github.com>
2024-01-28 11:14:42 -0500

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,7 @@ publishing {
             create<MavenPublication>("maven") {
                 groupId = "celestialexpressions"
                 artifactId = "celestialexpressions"
-                version = "1.4.3"
+                version = "1.4.4"
 
                 from(components["java"])
             }
