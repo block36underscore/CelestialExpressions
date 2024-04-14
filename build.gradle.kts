2eac7a92d3bd4495a4d89cc241b52eb2aa5418db
block36_ <block36not33@gmail.com>
2024-04-13 22:01:06 -0400

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ tasks.jar {
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
+    id("org.jetbrains.kotlin.jvm") version "1.8.20-Beta"
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
